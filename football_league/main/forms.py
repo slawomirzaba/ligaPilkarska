822c950f7f5993c0f770582efe9d5995187ab6be
slawomirzaba <slawekzaba@poczta.fm>
2016-06-03 13:23:11 +0200

    sortowanie w polach select

--- football_league/main/forms.py
+++ football_league/main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from main.models import *
 from django.forms.extras.widgets import SelectDateWidget
 
@@ -20,24 +21,24 @@ class CustomEventTypeChoiceField(forms.ModelChoiceField):
 
 class CustomPersonChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
-         return obj.name + " " + obj.surname
+         return obj.surname + " " + obj.name
 
 class CustomGameChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
          return "{0} : {1} - {2}".format(obj.number_of_queue, obj.host.name, obj.guest.name)
 
 class PersonForm(forms.ModelForm):
-    club = CustomClubChoiceField(queryset=Club.objects.all())
+    club = CustomClubChoiceField(queryset=Club.objects.order_by('name'))
 
     class Meta:
         model = Person
         fields = '__all__'
 
 class GameForm(forms.ModelForm):
-    host = CustomClubChoiceField(queryset=Club.objects.all())
-    guest = CustomClubChoiceField(queryset=Club.objects.all())
+    host = CustomClubChoiceField(queryset=Club.objects.order_by('name'))
+    guest = CustomClubChoiceField(queryset=Club.objects.order_by('name'))
     date = forms.DateField(input_formats=None, label='Data',
-                                    widget=SelectDateWidget(years=[y for y in range(1930, 2017)]))
+                                    widget=SelectDateWidget(years=[y for y in range(2015, 2017)]), initial=timezone.now())
 
     class Meta:
         model = Game
@@ -45,22 +46,22 @@ class GameForm(forms.ModelForm):
 
 class EventForm(forms.ModelForm):
     type = CustomEventTypeChoiceField(queryset=Types_of_event.objects.all())
-    person = CustomPersonChoiceField(queryset=Person.objects.all())
-    game = CustomGameChoiceField(queryset=Game.objects.all())
+    person = CustomPersonChoiceField(queryset=Person.objects.order_by('surname'))
+    game = CustomGameChoiceField(queryset=Game.objects.order_by('number_of_queue'))
     class Meta:
         model = Event
         fields = '__all__'
 
 class Member_of_gameForm(forms.ModelForm):
-    member = CustomPersonChoiceField(queryset=Person.objects.all())
-    games = customMultipleGamesChoiceField(queryset=Game.objects.all(), widget=forms.CheckboxSelectMultiple)
+    member = CustomPersonChoiceField(queryset=Person.objects.order_by('surname'))
+    games = customMultipleGamesChoiceField(queryset=Game.objects.order_by('number_of_queue'), widget=forms.CheckboxSelectMultiple)
     roles = customMultipleRolesChoiceField(queryset=Roles_in_game.objects.all(), widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Member_of_game
         fields = '__all__'
 
 class Member_of_game_edit_Form(forms.ModelForm):
-    games = customMultipleGamesChoiceField(queryset=Game.objects.all(), widget=forms.CheckboxSelectMultiple)
+    games = customMultipleGamesChoiceField(queryset=Game.objects.order_by('number_of_queue'), widget=forms.CheckboxSelectMultiple)
     roles = customMultipleRolesChoiceField(queryset=Roles_in_game.objects.all(), widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Member_of_game
@@ -69,7 +70,7 @@ class Member_of_game_edit_Form(forms.ModelForm):
 class ClubForm(forms.ModelForm):
     name = forms.CharField(max_length=45, min_length=6, strip=True, label='Nazwa')
     creation_date = forms.DateField(input_formats=None, label='Data powstania',
-                                    widget = SelectDateWidget(years=[y for y in range(1930,2017)]))
+                                    widget = SelectDateWidget(years=[y for y in range(1930,2017)]), initial=timezone.now())
     location = forms.CharField(max_length=45, min_length=6, strip=True, label='Miejscowosc')
 
     class Meta:

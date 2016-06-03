31e601365cd949bc2a3d68c5deb5fb08c7597d6c
mat183 <mateow1994@gmail.com>
2016-06-04 01:23:34 +0200

    Dodana walidacja dotyczaca sprawdzania czy dany mecz juz wystapil podczas dodawania do tabeli

--- football_league/main/views/Tables_management_games_add.py
+++ football_league/main/views/Tables_management_games_add.py
@@ -16,9 +16,24 @@ class Tables_management_games_add(View):
 		if form.is_valid():
 			host = form.cleaned_data['host']
 			guest = form.cleaned_data['guest']
+			queue = form.cleaned_data['number_of_queue']
+			proper_game = [game for game in Game.objects.all() if game.host == host and game.guest == guest]
+			converted_squads = [game for game in Game.objects.all() if game.host == guest and game.guest == host]
+						
 			if host == guest:
 				message = 'Blad! Pola gospodarza i goscia musza byc rozne!'
 				return render(request, 'main/tables_management_games_add.html', {'form': form, 'error': message})
+			
+			elif proper_game:
+				message = "Blad! Istnieje juz taki mecz w lidze"
+				return render(request, 'main/tables_management_games_add.html', {'form': form, 'error': message})
+
+			elif converted_squads:
+				game = converted_squads[0]
+				if queue == game.number_of_queue:
+					message = "Blad! Niewlasciwy numer kolejki dla meczu!"
+					return render(request, 'main/tables_management_games_add.html', {'form': form, 'error': message})
+
 			form.save()
 			return redirect("tables_management_games")
 		else:
